0e9dc55d5af65eee440b27066ca527980e2df56f
HatemReda7 <hreda2567@gmail.com>
2024-02-27 15:01:37 +0200

    added firebase firestore package

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'Layout/home_screen.dart';
 import 'Provider/my_provider.dart';
+import 'firebase_options.dart';
 import 'pref_helper.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   PrefsHelper.prefs = await SharedPreferences.getInstance();
   runApp(ChangeNotifierProvider(
       create: (context) =>
